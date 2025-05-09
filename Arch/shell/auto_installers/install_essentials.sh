5e22390e1675436baf58705df8c83bf5a4af8acb
Izeno <78334105+VoxDroid@users.noreply.github.com>
2025-05-09 21:44:17 +0900

    Update install_essentials.sh

--- Arch/shell/auto_installers/install_essentials.sh
+++ Arch/shell/auto_installers/install_essentials.sh
@@ -56,7 +56,8 @@ pacman -S --needed --noconfirm \
     gwenview dolphin \
     terminator tree \
     vim neovim \
-    neofetch tmux || log_error "Failed to install essential applications"
+    neofetch tmux \
+    networkmanager || log_error "Failed to install essential applications"
 
 log_info "Installing Brave browser..."
 sudo -u "$SUDO_USER" yay -S --needed --noconfirm brave-bin || log_error "Failed to install Brave"
