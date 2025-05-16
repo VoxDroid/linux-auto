c672394d35b39cf2aaf6485e85601a006d3a8adc
Izeno <78334105+VoxDroid@users.noreply.github.com>
2025-05-16 12:51:33 +0900

    Update install_shell.sh

--- Arch/shell/auto_installers/install_shell.sh
+++ Arch/shell/auto_installers/install_shell.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Installs shell enhancements for Arch Linux (Zsh, Fish, Powerline, etc.)
+# Installs shell enhancements for Arch Linux (Zsh, Fish, Powerline, Oh My Zsh, Powerlevel10k, etc.)
 # Run with sudo: sudo bash install_shell_enhancements.sh
 
 set -e
@@ -22,7 +22,7 @@ if [[ -z "$SUDO_USER" ]]; then
     log_error "SUDO_USER not set. Please run this script with sudo."
 fi
 
-read -p "Install shell enhancements (Zsh, Fish, etc.)? (y/N): " confirm
+read -p "Install shell enhancements (Zsh, Fish, Oh My Zsh, Powerlevel10k, etc.)? (y/N): " confirm
 if [[ ! "$confirm" =~ ^[Yy]$ ]]; then
     log_error "Script execution cancelled"
 fi
@@ -36,9 +36,31 @@ pacman -S --needed --noconfirm pacman-contrib || log_error "Failed to install pa
 log_info "Installing shell enhancements..."
 pacman -S --needed --noconfirm \
     zsh fish zsh-autosuggestions zsh-completions zsh-syntax-highlighting \
-    powerline powerline-fonts tmux || log_error "Failed to install shell enhancements"
+    powerline powerline-fonts tmux curl git || log_error "Failed to install shell enhancements"
 
 log_info "Cleaning package cache..."
 paccache -r || log_warn "Failed to clean package cache"
 
-log_info "Shell enhancements installation complete! Run 'chsh -s /usr/bin/zsh' or 'chsh -s /usr/bin/fish' to switch shells."
+log_info "Installing Oh My Zsh for user $SUDO_USER..."
+if [[ -d "/home/$SUDO_USER/.oh-my-zsh" ]]; then
+    log_info "Oh My Zsh already installed, skipping..."
+else
+    su - "$SUDO_USER" -c "sh -c \"\$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)\" --unattended" || log_error "Failed to install Oh My Zsh"
+fi
+
+log_info "Installing Powerlevel10k for user $SUDO_USER..."
+if [[ -d "/home/$SUDO_USER/.oh-my-zsh/custom/themes/powerlevel10k" ]]; then
+    log_info "Powerlevel10k already installed, skipping..."
+else
+    su - "$SUDO_USER" -c "git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/$SUDO_USER/.oh-my-zsh/custom/themes/powerlevel10k" || log_error "Failed to install Powerlevel10k"
+    su - "$SUDO_USER" -c "sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"powerlevel10k\/powerlevel10k\"/g' /home/$SUDO_USER/.zshrc" || log_warn "Failed to set Powerlevel10k theme"
+fi
+
+log_info "Enabling Zsh plugins for user $SUDO_USER..."
+su - "$SUDO_USER" -c "echo 'source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh' >> /home/$SUDO_USER/.zshrc" || log_warn "Failed to enable zsh-autosuggestions"
+su - "$SUDO_USER" -c "echo 'source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> /home/$SUDO_USER/.zshrc" || log_warn "Failed to enable zsh-syntax-highlighting"
+su - "$SUDO_USER" -c "echo 'fpath=(/usr/share/zsh/site-functions /usr/share/zsh/functions/Completion/* \$fpath)' >> /home/$SUDO_USER/.zshrc" || log_warn "Failed to enable zsh-completions"
+
+log_info "Shell enhancements, Oh My Zsh, and Powerlevel10k installation complete!"
+log_info "Run 'chsh -s /usr/bin/zsh' or 'chsh -s /usr/bin/fish' to switch shells."
+log_info "Powerlevel10k is set as the Zsh theme with completions enabled. Run 'p10k configure' to customize it."
